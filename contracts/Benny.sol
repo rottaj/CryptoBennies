f58145ead19c7bc741fb228e1c231d7aade883ef
rottaj <jackrotta12@gmail.com>
2021-09-08 18:31:00 -0400

    working

--- contracts/Benny.sol
+++ contracts/Benny.sol
@@ -10,13 +10,26 @@ contract Benny is IERC20 {
     mapping (address => uint) _addressBalances;
 
     constructor() {
-        _mint(msg.sender, _maxBennies);
+        _mint(msg.sender, (_maxBennies / 2));
         _name = "Bennies!";
     }
 
     function name() public pure returns (string memory name) {
         return _name;
     }
+    
+    function balanceOf(address _owner) public override view returns (uint balance) {
+        return _addressBalances[_owner];
+    }
+
+    function transfer(address _to, uint _amount) public override returns (bool success) {
+        _transfer(msg.sender, _to, _amount);
+    }
+    /**
+    function allowance(address _owner, address _spender) public override returns (uint256 remaining) {
+        
+    }
+     */ 
 
     function _transfer(address _to, address _from, uint _amount) internal {
         require(_addressBalances[_from] >= _amount);
@@ -28,5 +41,7 @@ contract Benny is IERC20 {
     function _mint(address _owner, uint _amount) internal virtual {
         require(_owner != address(0));
         require((_amount + _totalSupply) <= _maxBennies);
+        _addressBalances[_owner] += _amount;
+        _totalSupply += _amount;
     }
 }
